05b532a67fc74660afa0c7a465691b4cf22a2870
EdvardsMekss <edvards.mekss@gmail.com>
2023-02-02 12:02:01 +0200

    Create oneHand function that play one oneHand
    The function checks wether user input is valid, if not
    asks for it again and then compares it to computer choice
    and declares a draw or a winner

--- mainscript.js
+++ mainscript.js
@@ -1,34 +1,41 @@
-//1. - Make computer randomly select rock, paper or scissors
-    //1.1 - create a variable 'computerChoice' inside a function getComputerChoice
-    //1.2 - generate a random, whole number between 1 - 3
-    //1.3 - if number is 1 assign 'rock' to 'computerChoice', if 2 assign 'paper', if 3 assign 'scissors'
-    //1.4 - return 'computerChoice'
-
-//2. - Allow user to select rock, paper or scissors
-    //2.1 - create a variable 'playerCHoice' inside a function playerChoice
-    //2.2 - generate a popup box that takes input
-    //2.3 - store user input in a variable 'playerChoice'
-    //2.4 - return 'playerChoice'
-
-//3. - Compare computer and user selection and declare winner inside  a function game
-    //3.1 - Convert user input to lowercase and check wether it matches rock paper or scissors
-    //3.2 - if no, ask to repeat the input, repeat 3.1 untill a match is found
-    //3.3 - once a match is found compare against computer choice and return winner
-
-
-
+//Make computer randomly select rock, paper or scissors
 function getComputerChoice(){
+    //Generate a random, whole number between 1 - 3
     let randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1)
     let computerChoice
+    //If number is 1 assign 'rock' to 'computerChoice', if 2 assign 'paper', if 3 assign 'scissors'
     if (randomNumber === 1) computerChoice = 'rock'
     else if (randomNumber === 2) computerChoice = 'paper'
     else if (randomNumber === 3) computerChoice = 'scissors'
     return computerChoice
 }
 
-function playerChoice(){
-    let playerChoice = prompt('Type the desired hand: ')
-    return playerChoice
+//Take user input for rock, paper or scissors, convert to lowercase
+function playerChoice() {
+    let newPlayerHand = prompt("Enter rock, paper, or scissors:").toLowerCase();
+    return newPlayerHand;
 }
 
+//Compare computer and user selection and declare winner
+function oneHand(computerHand, playerHand) {
+    //Check wether it matches rock paper or scissors, if not, make user repeat input
+    let newPlayerHand = playerChoice();
+    const validOptions = ["rock", "paper", "scissors"];
 
+    while (!validOptions.includes(newPlayerHand)) {
+        newPlayerHand = playerChoice();
+    }
+    
+    //Compare user hand with computer hand and declare a winner or a draw
+    if (computerHand === newPlayerHand) return 'Draw!'
+
+    else if ((computerHand === 'rock' && newPlayerHand === 'scissors') ||
+             (computerHand === 'scissors' && newPlayerHand === 'paper')) return 'Computer Wins! ' + computerHand + ' beats ' + newPlayerHand
+    
+    else if ((computerHand === 'paper' && newPlayerHand === 'scissors') ||
+             (computerHand === 'scissors' && newPlayerHand === 'rock') ||
+             (computerHand === 'rock' && newPlayerHand === 'paper')) return 'Player Wins! ' + newPlayerHand + ' beats ' + computerHand
+    else return 'Unexpected hands: ' + newPlayerHand + ' ' + computerHand
+}
+
+console.log(oneHand(getComputerChoice(), playerChoice()))
